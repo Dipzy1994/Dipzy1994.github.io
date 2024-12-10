97c157ce328a425d380e9351f7c0f29e79ca5241
Dipzy1994 <119319924+Dipzy1994@users.noreply.github.com>
2024-12-10 17:54:58 +1100

    build update 0.0.2

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavMenu from './components/NavMenu';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import SignUp from './components/SignUp';
+
+const Home = lazy(() => import('./components/Home'));
+const About = lazy(() => import('./components/About'));
+const Services = lazy(() => import('./components/Services'));
+const Contact = lazy(() => import('./components/Contact'));
+const SignUp = lazy(() => import('./components/SignUp'));
 
 function App() {
   return (
     <Router>
       <div>
         <NavMenu />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/services" component={Services} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/signup" component={SignUp} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/services" component={Services} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
+        </Suspense>
       </div>
     </Router>
   );

bf4e6269506aebd211db45297cdfbb6eda9f4bd7
Dipzy1994 <119319924+Dipzy1994@users.noreply.github.com>
2024-12-10 17:33:21 +1100

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,27 +1,27 @@
-import React from 'react';  
-import Header from '../components/Header'; 
-import AboutUs from '../components/AboutUs';  
-import Events from '../components/Events';  
-import Signup from '../components/Signup';
-import ContactUs from '../components/ContactUs';  
-import Footer from '../components/Footer';  
-import WeatherDashboard from '../components/WeatherDashboard';  
-import ErrorBoundary from '../components/ErrorBoundary';  
-  
-function App() {  
-  return (  
-   <div>  
-    <ErrorBoundary>  
-      <Header />  
-      <AboutUs />  
-      <Events />  
-      <SignUp />  
-      <ContactUs />  
-      <WeatherDashboard />  
-      <Footer />  
-    </ErrorBoundary>  
-   </div>  
-  );  
-}  
-  
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavMenu from './components/NavMenu';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavMenu />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
 export default App;

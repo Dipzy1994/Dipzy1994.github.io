fd6bfe6f6233e48f8081031ae0071743c7f1dbd4
Dipzy1994 <119319924+Dipzy1994@users.noreply.github.com>
2024-12-10 19:31:40 +1100

    0.0.3

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+// Set the mode to 'production' or 'development or none'
+module.exports = {
+  mode: 'production', 
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+  },
+};

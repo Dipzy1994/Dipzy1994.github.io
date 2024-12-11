01e2698d19bda814d251d97c75bcea95a9e9384d
Dipzy1994 <119319924+Dipzy1994@users.noreply.github.com>
2024-12-11 19:34:04 +1100

    0.0.5

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../../styles/Header.module.css';
-import logo from '../../assets/logo.png'; // Make sure to add your logo image to the assets folder
+import logo from '../../assets/92e154aa-0919-41b7-b398-23f6acba8c51_3.jpeg'; // Make sure to add your logo image to the assets folder
 
 const Header = () => {
   return (
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,9 @@
  import React from 'react';
- import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-
- // Styles
- import '../styles/App.css';
-
- // Components
+ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+ import Header from '../components/layout/Header';
  import NavMenu from '../components/layout/NavMenu';
  import Footer from '../components/layout/Footer';
  import SearchBar from '../components/shared/SearchBar';
-
  import Home from '../components/Home';
  import About from '../components/About';
  import Teams from '../components/Teams';
@@ -20,14 +15,28 @@
  import SearchResults from '../components/SearchResults';
  import ErrorBoundary from '../components/ErrorBoundary';
 
-
-
  function App() {
    return (
      <Router>
-       <div className="app">
-         {/* Your existing app content */}
-       </div>
+       <ErrorBoundary>
+         <div className="App">
+           <Header />
+           <NavMenu />
+           <SearchBar />
+           <Routes>
+             <Route path="/" element={<Home />} />
+             <Route path="/about" element={<About />} />
+             <Route path="/teams" element={<Teams />} />
+             <Route path="/events" element={<Events />} />
+             <Route path="/contact" element={<Contact />} />
+             <Route path="/weather" element={<WeatherDashboard />} />
+             <Route path="/services" element={<Services />} />
+             <Route path="/signup" element={<Signup />} />
+             <Route path="/search" element={<SearchResults />} />
+           </Routes>
+           <Footer />
+         </div>
+       </ErrorBoundary>
      </Router>
    );
  }
